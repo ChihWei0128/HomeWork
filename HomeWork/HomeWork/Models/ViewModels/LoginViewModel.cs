f4b1e652b470c09fe9cdc02dbbacfba5a1811c43
ChihWei0128 <chihwei.chan@gmail.com>
2017-07-17 16:53:20 +0800

    第二版

---
+++ HomeWork/HomeWork/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,27 @@
+﻿using HomeWork.ValidateAttribute;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HomeWork.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "帳號")]
+        [DataType(DataType.EmailAddress)]
+        [RegularExpression(@"\w.+\@\w.+")]
+        [NoIs("skilltree,demo,twMVC", ErrorMessage = "帳號內有不可使用的字串[skilltree, demo, twMVC]")]
+        public string Account { get; set; }
+
+        [Required]
+        [Display(Name = "密碼")]
+        [DataType(DataType.Password)]
+        [StringLength(20, MinimumLength = 4)]
+        public string Password { get; set; }
+
+        public string Message { get; set; }
+    }
+}
